01310af99f218b9122a61adb7031ba8b6f742bf7
JoannaTruszczynska <joanna.truszczynska@gmail.com>
2019-07-24 10:50:25 +0200

    display question functionality added

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,9 @@
 import connection
 
 question_list = connection.get_all_data(connection.QUESTION_PATH)
+
+def get_single_question(question_id, question_list):
+
+    for question in question_list:
+        if question['id'] == question_id:
+            return question
--- server.py
+++ server.py
@@ -11,6 +11,13 @@ def show_list():
                            question_list = data_manager.question_list,
                            QUESTION_HEADERS = connection.QUESTION_HEADERS)
 
+@app.route('/question/<question_id>')
+def show_question_and_answers(question_id: int):
+    single_question=data_manager.get_single_question(question_id, connection.get_all_data(connection.QUESTION_PATH))
+    return render_template('question.html',
+                           question_id=question_id,
+                           single_question=single_question,
+                           QUESTION_HEADERS=connection.QUESTION_HEADERS)
 
 
 if __name__ == '__main__':
